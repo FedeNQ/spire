21983e880956e1b2ff8e3b947556f019979d9df3
FedeNQ <fedenahuel07@gmail.com>
2023-07-18 17:19:03 -0300

    add KUBECTLVERSION to test-k8s script
    Signed-off-by: FedeNQ <fedenahuel07@gmail.com>

--- test/integration/test-k8s.sh
+++ test/integration/test-k8s.sh
@@ -5,4 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}" || fail-now "Unable to change to script directory"
 
 export SUITES=suites-k8s/*
+export KUBECTLVERSION=v1.25.3
 ./test.sh $1
