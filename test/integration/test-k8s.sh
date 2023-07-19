34c57cda5596661e1cfdad4be1ca514efa2b2d84
FedeNQ <fedenahuel07@gmail.com>
2023-07-19 09:18:00 -0300

    add KUBECTL & KINDVERSION
    Signed-off-by: FedeNQ <fedenahuel07@gmail.com>

--- test/integration/test-k8s.sh
+++ test/integration/test-k8s.sh
@@ -5,5 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}" || fail-now "Unable to change to script directory"
 
 export SUITES=suites-k8s/*
-export K8SIMAGE=kindest/node:v1.25.3
+KINDVERSION=v0.17.0
+KUBECTLVERSION=v1.25.3
+K8SIMAGE=kindest/node:v1.25.3
 ./test.sh $1
