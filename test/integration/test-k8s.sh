bb984a7b995326fe78576782f1826ee0b957be9f
FedeNQ <fedenahuel07@gmail.com>
2023-07-19 12:16:53 -0300

    fix
    Signed-off-by: FedeNQ <fedenahuel07@gmail.com>

--- test/integration/test-k8s.sh
+++ test/integration/test-k8s.sh
@@ -5,7 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}" || fail-now "Unable to change to script directory"
 
 export SUITES=suites-k8s/*
-# EXPORT KINDVERSION=v0.17.0
-# EXPORT KUBECTLVERSION=v1.25.3
-# EXPORT K8SIMAGE=kindest/node:v1.25.3
 ./test.sh $1
