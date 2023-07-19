5643aeac65df72df91e961347a41bc653b288bf2
FedeNQ <fedenahuel07@gmail.com>
2023-07-19 09:41:55 -0300

    add matrix to integration test
    Signed-off-by: FedeNQ <fedenahuel07@gmail.com>

--- test/integration/test-k8s.sh
+++ test/integration/test-k8s.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}" || fail-now "Unable to change to script directory"
 
 export SUITES=suites-k8s/*
-KINDVERSION=v0.17.0
-KUBECTLVERSION=v1.25.3
-K8SIMAGE=kindest/node:v1.25.3
+# EXPORT KINDVERSION=v0.17.0
+# EXPORT KUBECTLVERSION=v1.25.3
+# EXPORT K8SIMAGE=kindest/node:v1.25.3
 ./test.sh $1
